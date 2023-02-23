822aa959524d046631258b7af701489f95319074
Admin <admin.push@hackerrank.workspace>
2023-02-23 18:00:16 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import 'h8k-components';
 
-function Articles({articles}) {
+import Articles from './components/Articles';
+
+const title = "Sorting Articles";
+
+function App({articles}) {
+    const [articlesList, setArticlesList] = useState(articles);
+    
+    useEffect(() => {
+        sortByUpvotes(articles);
+    },[]);
+
+    
+    const sortByUpvotes = () => {
+      var newArticles = [];
+      Object.assign(newArticles, articlesList);
+      newArticles.sort((a, b) => {
+        if (a.upvotes > b.upvotes) {
+          return -1;
+        }
+        if (a.upvotes < b.upvotes) {
+          return 1;
+        }
+        return 0;
+      })
+
+      setArticlesList(newArticles);
+    }
+
+    const sortByDates = () => {
+      var newArticles = [];
+      Object.assign(newArticles, articlesList);
+      newArticles.sort((a, b) => {
+        const aDate = new Date(a.date);
+        const bDate = new Date(b.date);
+        if (aDate > bDate) {
+          return -1;
+        }
+        if (aDate < bDate) {
+          return 1;
+        }
+        return 0;
+      })
+
+      setArticlesList(newArticles);
+    }
 
     return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                  {
-                    articles.map((article, index) => 
-                    (
-                      <tr data-testid="article" key={index}>
-                        <td data-testid="article-title">{article.title}</td>
-                        <td data-testid="article-upvotes">{article.upvotes.toString()}</td>
-                        <td data-testid="article-date">{article.date}</td>
-                    </tr>
-                    ))
-                  }
-                
-                </tbody>
-            </table>
+        <div className="App">
+            <h8k-navbar header={title}></h8k-navbar>
+            <div className="layout-row align-items-center justify-content-center my-20 navigation">
+                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+                <button data-testid="most-upvoted-link" className="small" onClick={() => sortByUpvotes()}>Most Upvoted</button>
+                <button data-testid="most-recent-link" className="small" onClick={() => sortByDates()}>Most Recent</button>
+            </div>
+            <Articles articles={articlesList}/>
         </div>
     );
 
 }
 
-export default Articles;
+export default App;
